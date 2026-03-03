1bea365c37a93e3b670f35e2bc25ec885df84620
Cristian Goina <goinac@janelia.hhmi.org>
2026-03-03 13:37:28 -0500

    fixed multiscale to use relative downsampling

--- src/zarr_tools/multiscale.py
+++ src/zarr_tools/multiscale.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import re
 import scipy.ndimage as ndi
+import traceback
 import zarr
 
 from dask.array.core import normalize_chunks, slices_from_chunks
@@ -64,43 +65,31 @@ def create_multiscale(dataset_store: zarr.storage.StoreLike,
     def is_spatial_axis(axis:int) -> bool:
         return axis in spatial_axes.values()
 
-    absolute_scaling_factors = np.array([pow(2, source_dataset_level)
-                                        if is_spatial_axis(i) else 1
-                                        for i,_ in enumerate(source_dataset_shape)])
-    if source_dataset_level == 0:
-        level0_translation = source_dataset_translation
-        level0_scale = source_dataset_scale
-
-    else:
-        level0_scale = tuple(s / pow(2,source_dataset_level)
-                             if is_spatial_axis(i) else s
-                             for i,s in enumerate(source_dataset_scale))
-        level0_translation = tuple(t - (pow(2,source_dataset_level-1)-0.5)
-                               if is_spatial_axis(i) else t
-                               for i,t in enumerate(source_dataset_translation))
+    # we consider scalinng factors to be 1 for the first level from which we start downsizing
+    scaling_factors = np.array([1] * len(source_dataset_shape))
 
     logger.info((
         f'Source level: {source_dataset_level}, '
         f'source dataset path: {dataset_path}, '
         f'shape: {source_dataset_shape} '
-        f'source scaling factors: {absolute_scaling_factors} '
         f'source scale: {source_dataset_scale} '
         f'source translation: {source_dataset_translation} '
-        f'level0 scale: {level0_scale} '
-        f'level0 translation: {level0_translation} '
         f'preserve anisotropy: {preserve_anisotropy} '
     ))
 
     # open the multiscale group in append mode
     multiscale_group = zarr.open_group(store=dataset_store, path=parent_dataset_path, mode='a')
     dataset_arr = multiscale_group[current_dataset_subpath]
-    current_level_shape = dataset_shape = dataset_arr.shape
+    current_level = source_dataset_level
+    current_level_shape = dataset_arr.shape
+    current_level_translation = source_dataset_translation
+    current_level_scale = source_dataset_scale
 
     def next_level(match):
         level = int(match.group(1))
         return match.group(0).replace(str(level), str(level + 1), 1)
 
-    spatial_axes_mask = [is_spatial_axis(i) for i in range(len(dataset_shape))]
+    spatial_axes_mask = [is_spatial_axis(i) for i in range(len(source_dataset_shape))]
 
     nlevels = 0
     while True:
@@ -112,39 +101,40 @@ def create_multiscale(dataset_store: zarr.storage.StoreLike,
         new_level_path = dataset_regex.sub(next_level, current_dataset_subpath)
         new_level = int(dataset_regex.match(new_level_path).group(1))
 
-        relative_scaling_factors, found = _get_downsample_factors(
+        downsampling, found = _get_downsample_factors(
             new_level,
             current_level_shape,
             dataset_blocksize,
-            tuple(absolute_scaling_factors * level0_scale),
+            tuple(current_level_scale),
             spatial_axes_mask,
             preserve_anisotropy=preserve_anisotropy,
         )
         if not found:
             break
 
-        absolute_scaling_factors = absolute_scaling_factors * relative_scaling_factors
-        current_level_scale = tuple(absolute_scaling_factors * level0_scale)
-        current_level_translation = tuple([round((s0 * (dsf / 2 - 0.5)) + tr0, 3) if dsf > 1 else tr0
-                                                 for (s0, dsf, tr0)
-                                                 in zip(level0_scale, absolute_scaling_factors, level0_translation)])
-        current_level_shape_array = (dataset_shape / absolute_scaling_factors).astype(int)
-        current_level_shape = tuple(map(int, current_level_shape_array))
+        relative_scaling_factors  = np.array(downsampling)
+        new_scaling_factors = scaling_factors * relative_scaling_factors
+        new_level_scale = tuple(relative_scaling_factors * current_level_scale)
+        new_level_translation = tuple([round((s * (dsf - 1) / 2) + tr, 3) if dsf > 1 else tr
+                                                 for (s, dsf, tr)
+                                                 in zip(current_level_scale, relative_scaling_factors, current_level_translation)])
+        new_level_shape_array = (current_level_shape / relative_scaling_factors).astype(int)
+        new_level_shape = tuple(map(int, new_level_shape_array))
 
         logger.info((
-            f'Level: {new_level}, '
-            f'level dataset path: {parent_dataset_path}/{new_level_path}, '
-            f'dataset shape (l0 -> l{new_level}): {dataset_shape} -> {current_level_shape} '
-            f'level downsampling factors (rel/abs): {relative_scaling_factors} / {absolute_scaling_factors} '
-            f'level scale: {current_level_scale} '
-            f'level translation: {current_level_translation} '
+            f'New Level: {new_level}, '
+            f'new level dataset path: {parent_dataset_path}/{new_level_path}, '
+            f'new dataset shape (l{current_level} -> l{new_level}): {current_level_shape} -> {new_level_shape} '
+            f'level downsampling factors (rel/abs): {relative_scaling_factors} / {new_scaling_factors} '
+            f'level scale: {new_level_scale} '
+            f'level translation: {new_level_translation} '
         ))
 
         pyramid_attrs = add_new_dataset(
             pyramid_attrs,
             new_level_path,
-            scale_transform=current_level_scale,
-            translation_transform=current_level_translation
+            scale_transform=new_level_scale,
+            translation_transform=new_level_translation
         )
 
         logger.info((
@@ -157,7 +147,7 @@ def create_multiscale(dataset_store: zarr.storage.StoreLike,
                                else None)
         new_dataset_arr = multiscale_group.require_array(
             new_level_path,
-            shape=current_level_shape,
+            shape=new_level_shape,
             chunks=dataset_blocksize,
             dtype=dataset_arr.dtype,
             compressors=dataset_arr.compressors,
@@ -186,16 +176,22 @@ def create_multiscale(dataset_store: zarr.storage.StoreLike,
             res = client.map(downsample, part)
             for f, r in as_completed(res, with_results=True):
                 if f.cancelled():
-                    exc = f.exception()
-                    logger.exception(f'Level {new_level}: block processing exception: {exc}')
+                    tb = f.traceback()
+                    logger.error(f'Level {new_level} downsampling exception: {'\n'.join(traceback.format_tb(tb))}')
                     res = False
                 else:
                     logger.debug(f'Level {new_level}: Finished writing blocks {r}')
 
             logger.info(f'Finished level {new_level} partition {idx}')
 
-        current_dataset_subpath = new_level_path
         dataset_arr = new_dataset_arr
+        current_level = new_level
+        current_level_shape = new_level_shape
+        current_dataset_subpath = new_level_path
+        scaling_factors = new_scaling_factors
+        current_level_scale = new_level_scale
+        current_level_translation = new_level_translation
+        logger.info(f'Successfully downsampled level {current_level} to a {dataset_arr.shape} array ')
         nlevels = nlevels + 1
 
     # update multscales group attributes
@@ -204,22 +200,18 @@ def create_multiscale(dataset_store: zarr.storage.StoreLike,
     })
 
 
-def _get_downsample_factors(level:int,
+def _get_downsample_factors(target_level:int,
                             data_shape:Tuple[int, ...],
-                            target_shape:Tuple[int, ...],
+                            min_shape:Tuple[int, ...],
                             data_spacing:Tuple[float, ...],
                             spatial_axes_mask:List[bool],
                             anisotropy_threshold:float=0.1,
                             preserve_anisotropy:bool=False):
-
-    if all(not spatial_axes_mask[i] or data_shape[i] <= target_shape[i] for i in range(len(data_shape))):
+    if all(not spatial_axes_mask[i] or data_shape[i] <= min_shape[i] for i in range(len(data_shape))):
         return [1] * len(data_shape), False
 
-    data_size = np.array(data_shape) * data_spacing
     logger.info((
-        f'Level {level}: get downsample factors for shape {data_shape} to {target_shape}, '
-        f'spacing: {data_spacing}, spatial axes mask {spatial_axes_mask}, '
-        f'volume size: {data_size}'
+        f'Get downsample factors for level {target_level} from shape {data_shape} with spacing {data_spacing} '
     ))
     factors = [1] * len(data_shape)
     factors_changed = False
@@ -235,14 +227,13 @@ def _get_downsample_factors(level:int,
         anisotropies.append(anisotropy)
 
     for i in range(len(data_shape)):
-        if spatial_axes_mask[i] and data_shape[i] > target_shape[i] // 2:
+        if spatial_axes_mask[i] and data_shape[i] > min_shape[i] // 2:
             if abs(1 - anisotropies[i]) < anisotropy_threshold or preserve_anisotropy:
                 factors[i] = 2
                 factors_changed = True
 
     logger.info((
-        f'Level {level}: downsample factors for shape {data_shape} to {target_shape}, '
-        f'spacing: {data_spacing}, spatial axes mask {spatial_axes_mask}: {factors} '
+        f'Computed downsample factors for level {target_level} from shape {data_shape} with spacing {data_spacing}: {factors} '
     ))
     return np.array(factors).astype(int), factors_changed
 
